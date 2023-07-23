0b74031417d23bc0527ece5316a248a7836f1b0e
JaqueBRSilva <j.silvabroccolo@gmail.com>
2023-07-23 01:14:47 -0300

    feat: criação dos estilos dos botões

---
+++ src/constants/Styles.ts
@@ -0,0 +1,37 @@
+
+const STYLES = {
+
+    filledButton: {
+        borderRadius: 7,
+        backgroundColor: "#000",
+        height: 52,
+        justifyContent: 'center',
+        marginBottom: 16
+    },
+
+    outlinedButton: {
+        borderRadius: 7,
+        backgroundColor: '#FFF',
+        borderWidth: 2,
+        borderStyle: 'solid',
+        borderColor: '#000',
+        height: 52,
+        justifyContent: 'center'
+    },
+
+    lighText: {
+        color: '#FFF',
+        textAlign: 'center',
+        fontFamily: 'Roboto_900Black',
+        fontSize: 14
+    },
+
+    darkText: {
+        color: '#000',
+        textAlign: 'center',
+        fontFamily: 'Roboto_900Black',
+        fontSize: 14
+    }
+}
+
+export default STYLES;

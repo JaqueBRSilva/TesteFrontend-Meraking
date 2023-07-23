ecd351c27f4652bd0336418d14495c86e9922cd6
JaqueBRSilva <j.silvabroccolo@gmail.com>
2023-07-23 15:53:16 -0300

    style: add fonte `Comfortaa_400Regular`

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import { Comfortaa_500Medium, useFonts } from '@expo-google-fonts/comfortaa';
+import { Comfortaa_400Regular, Comfortaa_500Medium, useFonts } from '@expo-google-fonts/comfortaa';
 import { Roboto_400Regular, Roboto_700Bold, Roboto_900Black } from '@expo-google-fonts/roboto';
 import Routes from './src/routes';
 
 const App = () => {
 
   let [fontsLoaded] = useFonts({
+    Comfortaa_400Regular,
     Comfortaa_500Medium,
     Roboto_400Regular,
     Roboto_700Bold,

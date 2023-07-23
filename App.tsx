3ff702307feec6f93e6d30cca86a38445e059ab1
JaqueBRSilva <j.silvabroccolo@gmail.com>
2023-07-23 01:12:48 -0300

    feat: add 'expo-fonts'

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Comfortaa_500Medium, useFonts } from '@expo-google-fonts/comfortaa';
+import { Roboto_400Regular, Roboto_700Bold, Roboto_900Black } from '@expo-google-fonts/roboto';
+import Routes from './src/routes';
 
-export default function App() {
+const App = () => {
+
+  let [fontsLoaded] = useFonts({
+    Comfortaa_500Medium,
+    Roboto_400Regular,
+    Roboto_700Bold,
+    Roboto_900Black,
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Routes />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;

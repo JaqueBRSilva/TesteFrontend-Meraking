f7c6713bd210c3bc4fb004daeb9f4c0b5dfc00ae
JaqueBRSilva <j.silvabroccolo@gmail.com>
2023-07-23 15:54:08 -0300

    style: modificações nos estilos do botão

--- src/constants/Styles.ts
+++ src/constants/Styles.ts
@@ -2,20 +2,22 @@
 const STYLES = {
 
     filledButton: {
-        borderRadius: 7,
+        borderRadius: 6,
         backgroundColor: "#000",
         height: 52,
+        width: 'auto',
         justifyContent: 'center',
         marginBottom: 16
     },
 
     outlinedButton: {
-        borderRadius: 7,
+        borderRadius: 6,
         backgroundColor: '#FFF',
         borderWidth: 2,
         borderStyle: 'solid',
         borderColor: '#000',
         height: 52,
+        width: 'auto',
         justifyContent: 'center'
     },
 

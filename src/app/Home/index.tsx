ddaabf5f80e6d45a139bbc27beef1e55ae69a029
JaqueBRSilva <j.silvabroccolo@gmail.com>
2023-07-23 22:14:13 -0300

    style: ajuste largura de img's na listagem inicial

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -28,7 +28,6 @@ const Home = () => {
                 </View>
             </View>
 
-
             <View style={styles.actionButtons}>
                 <Buttons
                     styleButton={STYLES.filledButton}
@@ -46,22 +45,13 @@ const Home = () => {
             </View>
 
             <View style={styles.postsArea}>
-
                 <Image
-                    style={{
-                        width: 167,
-                        height: 220
-                    }}
-                    resizeMode="cover"
+                    style={styles.imagesList}
                     source={{ uri: 'https://s3-alpha-sig.figma.com/img/2ade/0d98/e2d017549828f132dd9448107195c2c4?Expires=1690761600&Signature=c9pFb1yDeZftvxP62-b71Nv0xKWTUVbXQiF6YUtTeb0zSOd~XzFtb6fD2c-qeE5WgXEEnpEh8CtjDor9JggttyhZd9DSBn4dexj6F3U3c-MAqRRspifDFI3~PbHfURxqzDPddgKLMrKnG5fRIm6pdodEgtlqrwCIc5HpS~AwhSdX1DGo4rDL3gEtxkmGCFl-df5zBECIb4ti61~5jxvT~11nfJ0~b1hGZrgzXy5XznimVqnIqoYpoTy6rMp~pmlxyC6jwSVTNRdAgN2Y4oKMF9x~edURkL6yTmGZLqilkHV2Ed-12nHLfYYT0OMeeLYDSjiNiaDUKeC9zlPxRnP1SQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4' }}
                 />
 
                 <Image
-                    style={{
-                        width: 167,
-                        height: 310
-                    }}
-                    resizeMode="cover"
+                    style={styles.imagesList}
                     source={{ uri: 'https://s3-alpha-sig.figma.com/img/4617/a4ed/0486a26c76e2f11f0a3c218ce8b03e0f?Expires=1690761600&Signature=MrU4p8w9YZ922OrWaWXdaTRHdkmrdk4KswXJulxdKpksp4H2k-3q8awV1mJgW33bTftZlmRRLMccjU7u3gN6BXNkwanT8~7-CtXWJMq6k-oe2Rk8Nl3rsDoGRPHmDCDV3LbBOznvO~DY25Yw3CyLJZXianY0QbDxc5zyf2doDvljuQAD4LrNnjtYpfHCYS63D3YkSH7Sjs-C6Q0OhlHKahdUvKlpQIACUeeysiuGMjEg4RJu8UdODuZNtktRnpYYuZOW0rhbGaap8M-cafDCnDjfbh1TG3HN2WDm~rFMPlNZ80q7w0IuRA7OLuxnbq26HwM-hWZv1XorQkMiLbPl4A__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4' }}
                 />
             </View>
@@ -107,7 +97,14 @@ const styles = StyleSheet.create({
     postsArea: {
         flexDirection: "row",
         gap: 9,
-        justifyContent: 'space-between'
+    },
+
+    imagesList: {
+        flex: 1,
+        minWidth: 167,
+        // minHeight: 265,
+        minHeight: '45%',
+        resizeMode: 'cover',
     }
 
 })
